1ba2f5629a8dce10f7a8cd045d69eabca171c21f
Ivan Delija <idelija92@gmail.com>
2024-11-19 16:58:49 +0000

    Changed @RestController to @Controller to map requests to HTML templates instead of raw data

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class DemoApplication {
 
 	private final List<Petition> petitions = new ArrayList<>();
